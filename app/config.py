671ce764770e77e9e467798676e27f65f85a39b4
Alexander Shirapov <cccp2006_06@mail.ru>
2023-02-08 01:01:49 +0800

    feat: include settings

---
+++ app/config.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+from typing import Any, Dict
+
+from pydantic import BaseSettings
+
+
+class AppSettings(BaseSettings):
+    debug: bool = False
+    title: str = "Wallet Cards API"
+    version: str = "0.1.0"
+
+    @property
+    def fastapi_kwargs(self) -> Dict[str, Any]:
+        return {"debug": self.debug, "title": self.title, "version": self.version}
+
+
+@lru_cache
+def get_app_settings() -> AppSettings:
+    """Returns a cached instance of the AppSettings object."""
+    return AppSettings()
--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from app.routes import router
+from app.config import get_app_settings
 
 
 def get_application() -> FastAPI:
-    application = FastAPI()
+    settings = get_app_settings()
+    application = FastAPI(**settings.fastapi_kwargs)
     application.include_router(router)
     return application
 
