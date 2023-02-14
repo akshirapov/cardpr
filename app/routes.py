552d7533ffad19bf542525637783f30a2791b386
Alexander Shirapov <cccp2006_06@mail.ru>
2023-02-14 22:34:14 +0800

    feat!: main api endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter
 
+from app.services import add_balance, create_customer, read_balance
+
 router = APIRouter()
 
 
-@router.get("/")
-def root():
-    return {"status": "success", "data": None}
+ALLOWED_METHODS = ("addBalance", "createCustomer", "readBalance")
+
+
+@router.post("/webhook")
+def webhook(hook_data):
+    method = hook_data.get("method", "")
+    if method not in ALLOWED_METHODS:
+        return {"error": f"unsupported method <{method}>"}
+
+    if method == "addBalance":
+        return add_balance(hook_data)
+    elif method == "createCustomer":
+        return create_customer(hook_data)
+    elif method == "readBalance":
+        return read_balance(hook_data)
---
+++ app/services.py
@@ -0,0 +1,14 @@
+
+def create_customer(data):
+    """Creates a customer."""
+    return {}
+
+
+def read_balance(data: dict):
+    """Returns loyality system info."""
+    return {}
+
+
+def add_balance(data: dict):
+    """Accrues welcome and referral bonuses."""
+    return {}
