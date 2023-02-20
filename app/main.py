bf6fc631f69d2975a57d79a2a78504e88b0fbc59
Alexander Shirapov <cccp2006_06@mail.ru>
2023-02-21 00:07:14 +0800

    feat: add cors for security

--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from app.routes import router
 from app.config import get_app_settings
+from app.routes import router
 
 
 def get_application() -> FastAPI:
     settings = get_app_settings()
+
     application = FastAPI(**settings.fastapi_kwargs)
+
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     application.include_router(router)
+
     return application
 
 
